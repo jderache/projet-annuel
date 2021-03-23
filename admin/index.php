6d47679d2ba29a0ae12f46fe45bb71fb63aacc3e
Julien <derachejulien@orange.fr>
2021-03-23 12:37:27 +0100

    ajout de fichiers

---
+++ admin/index.php
@@ -0,0 +1,24 @@
+<?php
+echo "Bienvenue sur l'espace dédiée";
+
+// faire une foonction qui prend en param un age en 
+
+// echo "<br>";
+
+// function majorite(){
+// 	$age = 18;
+
+// 	if ($age>=18)
+// 	{
+// 		echo "Vous êtes majeur";
+// 	}
+// 	else if($age<18)
+// 	{
+// 		echo "Vous êtes mineur";
+// 	}
+
+// }
+
+// majorite();
+
+?>
