24139c4bbefb13370a0e029366ee65e079ff152a
Julien DERACHE <73697284+jderache@users.noreply.github.com>
2021-04-04 19:51:11 +0200

    2EME SPRINT
    Finalisation de l'interface HTML/CSS du répertoire protégé, ainsi que de la page d'accueil.
    
    Ajout d'un formulaire de contact + fonction pour télécharger les CV des membres de la promotion.

--- admin/index.php
+++ admin/index.php
@@ -1,24 +1,166 @@
-<?php
-echo "Bienvenue sur l'espace dédiée";
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Les bêtas testeurs</title>
+<!-- CSS -->
+<style>
+  body {
+    display: grid;
+    background-color: #130025;
+    font-family: RedHatDisplay;
+  }
+  @font-face {
+    font-family: RedHatDisplay;
+    src: url(font/RedHatDisplay-Black.ttf);
+  }
+  @font-face {
+    font-family: Lexend;
+    src: url(font/Lexend-Thin.ttf);
+  }
+  .header {
+    position: fixed;
+    left: 0;
+    right: 0;
+    height: 80px;
+    line-height: 80px;
+    background-color: #222222;
+  }
+  .footer {
+    position: fixed;
+    bottom: 0;
+    height: 50px;
+    line-height: 50px;
+    background-color: #222222;
+  }
+  .menu {
+    padding: 0 10px;
+    text-align: center;
+  }
+  .menu a {
+    font-family: Lexend;
+    font-weight: bold;
+    text-decoration: ;
+    text-transform: uppercase;
+    text-decoration: none;
+    padding: 0 10px;
+    color: #FFF;
+    transition: all 0.1s ease 0.2s;
+  }
+  .menu a:hover {
+    color: #62bcda;
+    -webkit-transition: color 1s;
+    transition: color 1s;
+  }
+  h1 {
+    font-size: 150px;
+    line-height: 1;
+    font-weight: bold;
+    letter-spacing: 1px;
+    text-transform: uppercase;
+    text-align: center;
+    color: #FFF;
+  }
+  .liste a {
+    font-size: 200%;
+    font-family: Lexend;
+    font-weight: bold;
+    text-transform: uppercase;
+    text-decoration: none;
+    padding: 0 10px;
+    color: #FFF;
+    transition: all 0.1s ease 0.2s;
+  }
+  .liste a:hover {
+    color: #62bcda;
+    -webkit-transition: color 1s;
+    transition: color 1s;
+  }
+</style>
+</head>
 
-// faire une foonction qui prend en param un age en 
-
-// echo "<br>";
-
-// function majorite(){
-// 	$age = 18;
-
-// 	if ($age>=18)
-// 	{
-// 		echo "Vous êtes majeur";
-// 	}
-// 	else if($age<18)
-// 	{
-// 		echo "Vous êtes mineur";
-// 	}
-
-// }
-
-// majorite();
-
-?>
+<body>
+	<!-- Navigation -->
+	<header class="header">
+		<nav class="menu">
+			<a href="https://www.je-code.com/esgi1/adavid28/annuaire">Retour vers l'Accueil</a>
+		</nav>
+	</header><!-- Titre -->
+	<h1><span>Qui êtes vous ?</span></h1><!-- Liste des étudiants -->
+	<center>
+		<table class="liste">
+			<tr>
+				<td>
+					<a href=""><b>Hugo BERLEUR</b></a><br>
+					<br>
+				</td>
+				<td>
+					<a href=""><b>Matthys BOUREAU</b></a><br>
+					<br>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<a href=""><b>Achille DAVID</b></a><br>
+					<br>
+				</td>
+				<td>
+					<a href=""><b>Julien DERACHE</b></a><br>
+					<br>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<a href=""><b>Hugo DOOM</b></a><br>
+					<br>
+				</td>
+				<td>
+					<a href=""><b>Thibaut DUSAUTOIR</b></a><br>
+					<br>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<a href=""><b>Elias EL KIHEL</b></a><br>
+					<br>
+				</td>
+				<td>
+					<a href=""><b>Erwan FONTAINE</b></a><br>
+					<br>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<a href=""><b>Quentin GRANSART</b></a><br>
+					<br>
+				</td>
+				<td>
+					<a href=""><b>Aurélien TRUFFIER</b></a><br>
+					<br>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<a href=""><b>Bahya KHEMISSI</b></a><br>
+					<br>
+				</td>
+				<td>
+					<a href=""><b>Anaël VEKEMANS</b></a><br>
+					<br>
+				</td>
+			</tr>
+		</table>
+	</center>
+	
+	<br>
+	<br>
+	
+	<footer class="footer">
+		<nav class="menu">
+			<a href="https://www.je-code.com/esgi1/adavid28/annuaire/contact.php">Nous contacter</a>
+		</nav>
+	</footer>
+</body>
+</html>
